ef7ed706c00b48e03accca3ab2e36b2e9775785f
kristokiis <kristo@gbfmedia.ee>
2011-05-08 09:53:05 -0700

    works now with 0.10 version

--- src/list.js
+++ src/list.js
@@ -5,35 +5,47 @@
 * Licensed unter the terms of http://www.aloha-editor.com/license.html
 */
 /**
- * Register the ListPlugin as GENTICS.Aloha.Plugin
+ * Register the ListPlugin as Aloha.Plugin
  */
-GENTICS.Aloha.ListPlugin = new GENTICS.Aloha.Plugin('list');
+
+// Start Closure
+(function(window, undefined) {
+	var
+		jQuery = window.alohaQuery, $ = jQuery,
+		GENTICS = window.GENTICS,
+		Aloha = window.Aloha;
+		
+	Aloha.List = new (Aloha.Plugin.extend({
+	_constructor: function(){
+		this._super('list');
+	},
+
 
 /**
  * Configure the available languages
  */
-GENTICS.Aloha.ListPlugin.languages = ['en', 'de', 'fr', 'eo', 'fi', 'ru', 'it'];
+languages: ['en', 'de', 'fr', 'eo', 'fi', 'ru', 'it'],
 
 /**
  * default button configuration
  */
-GENTICS.Aloha.ListPlugin.config = [ 'ul', 'ol' ];
+config: [ 'ul', 'ol' ],
 
 /**
  * List of transformable elements
  */
-GENTICS.Aloha.ListPlugin.transformableElements = {'p' : true, 'h1' : true, 'h2' : true, 'h3' : true, 'h4' : true, 'h5' : true, 'h6' : true, 'ul' : true, 'ol' : true};
+transformableElements: {'p' : true, 'h1' : true, 'h2' : true, 'h3' : true, 'h4' : true, 'h5' : true, 'h6' : true, 'ul' : true, 'ol' : true},
 
 /**
  * Initialize the plugin, register the buttons
  */
-GENTICS.Aloha.ListPlugin.init = function() {
+init: function() {
 
 	var that = this;
 
 	// the 'create unordered list' button
-	this.createUnorderedListButton = new GENTICS.Aloha.ui.Button({
-		'iconClass' : 'GENTICS_button GENTICS_button_ul',
+	this.createUnorderedListButton = new Aloha.ui.Button({
+		'iconClass' : 'aloha-button aloha-button-ul',
 		'size' : 'small',
 		'tooltip' : this.i18n('button.createulist.tooltip'),
 		'toggle' : true,
@@ -42,16 +54,16 @@ GENTICS.Aloha.ListPlugin.init = function() {
 		}
 	});
 	// add to floating menu
-	GENTICS.Aloha.FloatingMenu.addButton(
-		'GENTICS.Aloha.continuoustext',
+	Aloha.FloatingMenu.addButton(
+		'Aloha.continuoustext',
 		this.createUnorderedListButton,
-		GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
+		Aloha.i18n(Aloha, 'floatingmenu.tab.format'),
 		1
 	);
 
 	// the 'create ordered list' button
-	this.createOrderedListButton = new GENTICS.Aloha.ui.Button({
-		'iconClass' : 'GENTICS_button GENTICS_button_ol',
+	this.createOrderedListButton = new Aloha.ui.Button({
+		'iconClass' : 'aloha-button aloha-button-ol',
 		'size' : 'small',
 		'tooltip' : this.i18n('button.createolist.tooltip'),
 		'toggle' : true,
@@ -60,72 +72,72 @@ GENTICS.Aloha.ListPlugin.init = function() {
 		}
 	});
 	// add to floating menu
-	GENTICS.Aloha.FloatingMenu.addButton(
-		'GENTICS.Aloha.continuoustext',
+	Aloha.FloatingMenu.addButton(
+		'Aloha.continuoustext',
 		this.createOrderedListButton,
-		GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
+		Aloha.i18n(Aloha, 'floatingmenu.tab.format'),
 		1
 	);
 
 	// add the event handler for selection change
-	GENTICS.Aloha.EventRegistry.subscribe(GENTICS.Aloha, 'selectionChanged', function(event, rangeObject) {
+	Aloha.bind('aloha-selection-changed',function(event,rangeObject){
 		that.createUnorderedListButton.setPressed(false);
 		that.createOrderedListButton.setPressed(false);
 		for (var i = 0; i < rangeObject.markupEffectiveAtStart.length; i++) {
 			var effectiveMarkup = rangeObject.markupEffectiveAtStart[ i ];
-			if (GENTICS.Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, jQuery('<ul></ul>'))) {
+			if (Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, jQuery('<ul></ul>'))) {
 				that.createUnorderedListButton.setPressed(true);
 				break;
 			}
-			if (GENTICS.Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, jQuery('<ol></ol>'))) {
+			if (Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, jQuery('<ol></ol>'))) {
 				that.createOrderedListButton.setPressed(true);
 				break;
 			}
 		}
-		if (GENTICS.Aloha.activeEditable) {
-			that.applyButtonConfig(GENTICS.Aloha.activeEditable.obj);
+		if (Aloha.activeEditable) {
+			that.applyButtonConfig(Aloha.activeEditable.obj);
 		}
 
 		// TODO this should not be necessary here!
-		GENTICS.Aloha.FloatingMenu.doLayout();
+		Aloha.FloatingMenu.doLayout();
 	});
 
 	// add the key handler for Tab
-	GENTICS.Aloha.Markup.addKeyHandler(9, function(event) {
+	Aloha.Markup.addKeyHandler(9, function(event) {
 		return that.processTab(event);
 	});
-};
+},
 
 /**
  * Applys a configuration specific for an editable
  * buttons not available in this configuration are hidden
  * @param {jQuery} obj jQuery object of the activated editable
  */
-GENTICS.Aloha.ListPlugin.applyButtonConfig = function (obj) {
+applyButtonConfig: function (obj) {
 
 	var config = this.getEditableConfig(obj);
 
-	if (GENTICS.Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0]) {
+	if (Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0]) {
 		// show/hide them according to the config
-		if (jQuery.inArray('ul', config) != -1 && GENTICS.Aloha.Selection.canTag1WrapTag2(GENTICS.Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0].nodeName, "ul") != -1) {
+		if (jQuery.inArray('ul', config) != -1 && Aloha.Selection.canTag1WrapTag2(Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0].nodeName, "ul") != -1) {
 			this.createUnorderedListButton.show();
 		} else {
 			this.createUnorderedListButton.hide();
 		}
 
-		if (jQuery.inArray('ol', config) != -1 && GENTICS.Aloha.Selection.canTag1WrapTag2(GENTICS.Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0].nodeName, "ol") != -1) {
+		if (jQuery.inArray('ol', config) != -1 && Aloha.Selection.canTag1WrapTag2(Aloha.Selection.rangeObject.unmodifiableMarkupAtStart[0].nodeName, "ol") != -1) {
 			this.createOrderedListButton.show();
 		} else {
 			this.createOrderedListButton.hide();
 		}
 	}
-};
+},
 
 
 /**
  * Process Tab and Shift-Tab pressed in lists
  */
-GENTICS.Aloha.ListPlugin.processTab = function (event) {
+processTab: function (event) {
 	switch(event['keyCode']) {
 		case 9: // TAB
 			if (event.shiftKey) {
@@ -135,14 +147,14 @@ GENTICS.Aloha.ListPlugin.processTab = function (event) {
 			}
 	}
 	return true;
-};
+},
 
 /**
  * For the current selection, get the DOM object, which will be transformed to/from the list
  * @return dom object or false
  */
-GENTICS.Aloha.ListPlugin.getStartingDomObjectToTransform = function () {
-	var rangeObject = GENTICS.Aloha.Selection.rangeObject;
+getStartingDomObjectToTransform: function () {
+	var rangeObject = Aloha.Selection.rangeObject;
 
 	for (var i = 0; i < rangeObject.markupEffectiveAtStart.length; i++) {
 		var effectiveMarkup = rangeObject.markupEffectiveAtStart[ i ];
@@ -152,14 +164,14 @@ GENTICS.Aloha.ListPlugin.getStartingDomObjectToTransform = function () {
 	}
 
 	return false;
-};
+},
 
 /**
  * For the current selection, get the nearest list item as dom object
  * @return dom object or false
  */
-GENTICS.Aloha.ListPlugin.getNearestSelectedListItem = function () {
-	var rangeObject = GENTICS.Aloha.Selection.rangeObject;
+getNearestSelectedListItem: function () {
+	var rangeObject = Aloha.Selection.rangeObject;
 
 	for (var i = 0; i < rangeObject.markupEffectiveAtStart.length; i++) {
 		var effectiveMarkup = rangeObject.markupEffectiveAtStart[ i ];
@@ -169,22 +181,22 @@ GENTICS.Aloha.ListPlugin.getNearestSelectedListItem = function () {
 	}
 
 	return false;
-};
+},
 
 /**
  * Transform the current selection to/from a list
  * @param ordered true when transforming to/from an ordered list, false for unordered lists
  */
-GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
+transformList: function (ordered) {
 	var domToTransform = this.getStartingDomObjectToTransform();
 
 	if (!domToTransform) {
 		// wrap a paragraph around the selection
-		GENTICS.Aloha.Selection.changeMarkupOnSelection(jQuery('<p></p>'));
+		Aloha.Selection.changeMarkupOnSelection(jQuery('<p></p>'));
 		domToTransform = this.getStartingDomObjectToTransform();
 
 		if (!domToTransform) {
-			GENTICS.Aloha.Log.error(this, 'Could not transform selection into a list');
+			Aloha.Log.error(this, 'Could not transform selection into a list');
 			return;
 		}
 	}
@@ -208,7 +220,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 			// transform all li into p
 			var jqToTransform = jQuery(domToTransform);
 			jQuery.each(jqToTransform.children('li'), function(index, li) {
-				var newPara = GENTICS.Aloha.Markup.transformDomObject(li, 'p');
+				var newPara = Aloha.Markup.transformDomObject(li, 'p');
 				// if any lists are in the paragraph, move the to after the paragraph
 				newPara.after(newPara.children('ol,ul'));
 			});
@@ -220,7 +232,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 		// we are in an unordered list and shall transform it to an ordered list
 
 		// transform the ul into an ol
-		GENTICS.Aloha.Markup.transformDomObject(domToTransform, 'ol');
+		Aloha.Markup.transformDomObject(domToTransform, 'ol');
 
 		// merge adjacent lists
 		this.mergeAdjacentLists(jQuery(domToTransform));
@@ -228,7 +240,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 		// we are in an ordered list and shall transform it to an unordered list
 
 		// transform the ol into an ul
-		GENTICS.Aloha.Markup.transformDomObject(domToTransform, 'ul');
+		Aloha.Markup.transformDomObject(domToTransform, 'ul');
 
 		// merge adjacent lists
 		this.mergeAdjacentLists(jQuery(domToTransform));
@@ -248,7 +260,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 			// transform all li into p
 			var jqToTransform = jQuery(domToTransform);
 			jQuery.each(jqToTransform.children('li'), function(index, li) {
-				var newPara = GENTICS.Aloha.Markup.transformDomObject(li, 'p');
+				var newPara = Aloha.Markup.transformDomObject(li, 'p');
 				// if any lists are in the paragraph, move the to after the paragraph
 				newPara.after(newPara.children('ol,ul'));
 			});
@@ -260,7 +272,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 		// we are in something different from a list and shall transform it into a list
 
 		// get the also selected siblings of the dom object
-		var selectedSiblings = GENTICS.Aloha.Selection.rangeObject.getSelectedSiblings(domToTransform);
+		var selectedSiblings = Aloha.Selection.rangeObject.getSelectedSiblings(domToTransform);
 
 		// create a new list
 		var jqList = ordered ? jQuery('<ol></ol>') : jQuery('<ul></ul>');
@@ -283,7 +295,7 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 					}
 
 					// transform the block level element
-					jqNewLi = GENTICS.Aloha.Markup.transformDomObject(selectedSiblings[i], 'li');
+					jqNewLi = Aloha.Markup.transformDomObject(selectedSiblings[i], 'li');
 					jqList.append(jqNewLi);
 				} else {
 					if (selectedSiblings[i].nodeType == 3
@@ -305,12 +317,12 @@ GENTICS.Aloha.ListPlugin.transformList = function (ordered) {
 
 	// refresh the selection
 	this.refreshSelection();
-};
+},
 
 /**
  * Indent the selected list items by moving them into a new created, nested list
  */
-GENTICS.Aloha.ListPlugin.indentList = function () {
+indentList: function () {
 	var listItem = this.getNearestSelectedListItem();
 	if (listItem) {
 		var jqItemBefore = jQuery(listItem).prev('li');
@@ -323,7 +335,7 @@ GENTICS.Aloha.ListPlugin.indentList = function () {
 		var jqOldList = jQuery(listItem).parent();
 
 		// get the also selected siblings of the dom object
-		var selectedSiblings = GENTICS.Aloha.Selection.rangeObject.getSelectedSiblings(listItem);
+		var selectedSiblings = Aloha.Selection.rangeObject.getSelectedSiblings(listItem);
 
 
 		// create the new list element by cloning the selected list element's parent
@@ -350,12 +362,12 @@ GENTICS.Aloha.ListPlugin.indentList = function () {
 	}
 
 	return true;
-};
+},
 
 /**
  * Outdent nested list items by moving them into the outer list
  */
-GENTICS.Aloha.ListPlugin.outdentList = function () {
+outdentList: function () {
 	var listItem = this.getNearestSelectedListItem();
 	if (listItem) {
 		// check whether the list is nested into another list
@@ -373,7 +385,7 @@ GENTICS.Aloha.ListPlugin.outdentList = function () {
 			// the list is nested into another list
 
 			// get the also selected siblings of the dom object
-			var selectedSiblings = GENTICS.Aloha.Selection.rangeObject.getSelectedSiblings(listItem);
+			var selectedSiblings = Aloha.Selection.rangeObject.getSelectedSiblings(listItem);
 
 			// check for multiple selected items
 			if (selectedSiblings && selectedSiblings.length > 0) {
@@ -424,25 +436,25 @@ GENTICS.Aloha.ListPlugin.outdentList = function () {
 	}
 
 	return true;
-};
+},
 
 /**
  * Refresh the current selection and set to focus to the current editable again
  */
-GENTICS.Aloha.ListPlugin.refreshSelection = function () {
-	if (GENTICS.Aloha.activeEditable) {
-		GENTICS.Aloha.activeEditable.obj[0].focus();
+refreshSelection: function () {
+	if (Aloha.activeEditable) {
+		Aloha.activeEditable.obj[0].focus();
 	}
-	GENTICS.Aloha.Selection.rangeObject.update();
-	GENTICS.Aloha.Selection.rangeObject.select();
-	GENTICS.Aloha.Selection.updateSelection();
-};
+	Aloha.Selection.rangeObject.update();
+	Aloha.Selection.rangeObject.select();
+	Aloha.Selection.updateSelection();
+},
 
 /**
  * Merge adjacent lists (of same type) into the first list
  * @param jqList jQuery object of a list
  */
-GENTICS.Aloha.ListPlugin.mergeAdjacentLists = function (jqList) {
+mergeAdjacentLists: function (jqList) {
 	// first get the first previous sibling of same type
 	var firstList = jqList.get(0);
 	while (firstList.previousSibling && firstList.previousSibling.nodeType == 1
@@ -461,4 +473,6 @@ GENTICS.Aloha.ListPlugin.mergeAdjacentLists = function (jqList) {
 		}
 		jqNextList.remove();
 	}
-};
+}
+}))();
+})(window);
